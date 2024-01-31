045ed45c33c60584a17668e028a63cf4ed8d9f37
SebZG <sebastian.zapata.g@gmail.com>
2024-01-31 21:29:26 +0000

    add query params

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,35 @@
-var apiKey = "08bfac6db5a24fa780d937a91262a007";
-
-var queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=08bfac6db5a24fa780d937a91262a007";
+let query = "";
+let diet = "";
+let include = "";
+let exclude = "";
+let intolerances = "";
+let addRecipe = true;
+const apiKey = "08bfac6db5a24fa780d937a91262a007";
+var queryURL =
+  `
+https://api.spoonacular.com/recipes/complexSearch?&
+query=${query}
+&diet=${diet}
+&includeIngredients=${include}
+&excludeIngredients=${exclude}
+&intolerances=${intolerances}
+&addRecipeInformation=${addRecipe}
+&apiKey=${apiKey}
+`;
 
 
 fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
 
+const foodish = "https://foodish-api.com/api/"
 
-    
 // a GET request to the API endpoint
-fetch('https://foodish-api.com/api/')
+fetch(foodish)
   .then(response => response.json())
   .then(data => {
     // Log the image URL from the response
